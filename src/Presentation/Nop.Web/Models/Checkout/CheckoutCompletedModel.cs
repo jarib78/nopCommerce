2fe7843527a9d1b3da70fba0a0f680a3ee9d36b2
jarib78 <jarib208@gmail.com>
2026-08-10 17:52:15 -0600

    Replace checkout continue button with WhatsApp order link

--- src/Presentation/Nop.Web/Models/Checkout/CheckoutCompletedModel.cs
+++ src/Presentation/Nop.Web/Models/Checkout/CheckoutCompletedModel.cs
@@ -4,7 +4,22 @@ namespace Nop.Web.Models.Checkout;
 
 public partial record CheckoutCompletedModel : BaseNopModel
 {
+    public CheckoutCompletedModel()
+    {
+        Items = new List<CheckoutCompletedOrderItemModel>();
+    }
+
     public int OrderId { get; set; }
     public string CustomOrderNumber { get; set; }
     public bool OnePageCheckoutEnabled { get; set; }
+    public string OrderTotal { get; set; }
+    public string WhatsAppNumber { get; set; }
+    public IList<CheckoutCompletedOrderItemModel> Items { get; set; }
+}
+
+public partial record CheckoutCompletedOrderItemModel : BaseNopModel
+{
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public string AttributeInfo { get; set; }
 }
